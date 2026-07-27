4dad283d750142eb3ad28245e3beff330a57a3ab
arvoreser <e.arvoreser@gmail.com>
2026-07-26 22:03:18 -0300

    desing.js

---
+++ empresas/desing.js
@@ -0,0 +1,5 @@
+window.EMPRESA_CONFIG = {
+  "id": "desing",
+  "nome": "Desing Brilhos",
+  "appsScriptUrl": "https://script.google.com/macros/s/AKfycbyaEbwvC3OcItIqjo3aksJB880XAvNCss1ObYAbPSrVA2T6Zy4KpNR9T-veqJldhTkSfA/exec"
+};
