aab4a3f710c100ab43f60fe6f6762cc307026bba
arvoreser <e.arvoreser@gmail.com>
2026-08-17 22:35:05 -0300

    Força carregamento da versão atualizada da evolução da dor

--- dashboard/js/dashboard.js
+++ dashboard/js/dashboard.js
@@ -11,7 +11,7 @@ function carregarModuloEvolucaoDor() {
     }
 
     const script = document.createElement('script');
-    script.src = 'js/modules/evolucao-dor.js?v=20260818-1';
+    script.src = 'js/modules/evolucao-dor.js?v=20260817-2234';
     script.dataset.modulo = 'evolucao-dor';
     script.onload = () => {
       script.dataset.carregado = 'true';
