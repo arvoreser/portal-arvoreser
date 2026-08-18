9ef7d940fed3740915e6e1bd9adaf5353fb00772
arvoreser <e.arvoreser@gmail.com>
2026-08-17 23:32:00 -0300

    Ativa clique na reavaliação por setor

--- dashboard/js/dashboard.js
+++ dashboard/js/dashboard.js
@@ -46,6 +46,7 @@ async function carregarModulosEvolucao() {
   await carregarScriptModulo('js/modules/evolucao-estresse.js?v=20260817-2258','data-modulo','evolucao-estresse');
   await carregarScriptModulo('js/modules/evolucao-interferencia.js?v=20260817-2304','data-modulo','evolucao-interferencia');
   await carregarScriptModulo('js/modules/evolucao-gl.js?v=20260817-2328','data-modulo','evolucao-gl');
+  await carregarScriptModulo('js/modules/setor-reavaliacao.js?v=20260817-2342','data-modulo','setor-reavaliacao');
 }
 
 async function init() {
@@ -62,6 +63,7 @@ async function init() {
   renderList();
   renderPerson();
   renderCharts();
+  if(typeof window.ativarCliqueReavaliacaoSetor === 'function') window.ativarCliqueReavaliacaoSetor();
 
   if(typeof window.renderEvolucaoRegioes1Mes === 'function') window.renderEvolucaoRegioes1Mes();
   if(typeof window.renderEvolucaoEstresse1Mes === 'function') window.renderEvolucaoEstresse1Mes();
